081c64f9377d5427e5d04ddd6d6c8ee02a99d2e1
wookiexox <lukaszwator26@gmail.com>
2025-03-24 15:55:07 +0100

    Added first collision system

--- player.gd
+++ player.gd
@@ -1,14 +1,15 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	var velocity = Vector2.ZERO
 	
@@ -40,3 +41,15 @@ func _process(delta) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
